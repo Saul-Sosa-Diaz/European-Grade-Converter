ea26e9fd9f6571c9c321a90c955ff869898332ad
Saul Sosa <alu0101404141@ull.edu.es>
2024-09-24 19:17:08 +0100

    Finished

--- src/app/lib/countriesClasses/ireland-grade-converter.ts
+++ src/app/lib/countriesClasses/ireland-grade-converter.ts
@@ -3,23 +3,22 @@ type GradeRange = {
   min: number;
   max: number;
   base: number;
-  divisor: number;
 };
 export class IrelandGradeConverter implements ICountryConverter {
   private gradeRanges: GradeRange[] = [
-    { min: 40, max: 44, base: 5, divisor: 5 }, // Entre 40-45, base 5
-    { min: 45, max: 49, base: 6, divisor: 5 }, // Entre 45-50, base 6
-    { min: 50, max: 59, base: 7, divisor: 10 }, // Entre 50-60, base 7
-    { min: 60, max: 69, base: 8, divisor: 10 }, // Entre 60-70, base 8
-    { min: 70, max: 100, base: 9, divisor: 30 }, // Entre 70-100, base 9
+    { min: 40, max: 44, base: 5 }, // Entre 40-45, base 5
+    { min: 45, max: 49, base: 6 }, // Entre 45-50, base 6
+    { min: 50, max: 59, base: 7 }, // Entre 50-60, base 7
+    { min: 60, max: 69, base: 8 }, // Entre 60-70, base 8
+    { min: 70, max: 100, base: 9}, // Entre 70-100, base 9
   ];
   convertToDestinationCountry(grade: number): string {
     return grade.toFixed(2);
   }
   convertToSpain(grade: number): string {
-    const range = this.gradeRanges.find((r) => grade >= r.min && grade < r.max); // Find the range of the grade
+    const range = this.gradeRanges.find((r) => grade >= r.min && grade <= r.max); // Find the range of the grade
     if (!range) return "0"; // if the range is not found, return 0
-    const result = range.base + (grade - range.min) / range.divisor;
+    const result = range.base + (grade - range.min) / (range.max - range.min);
     return result.toFixed(2);
   }
 }
