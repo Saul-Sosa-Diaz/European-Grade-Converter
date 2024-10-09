8589fcfda72264886a8ca48ce6b2398b9b396d29
Saul Sosa <alu0101404141@ull.edu.es>
2024-10-09 14:32:49 +0100

    Fixed problems with the styles

--- src/app/components/country-additional-information.tsx
+++ src/app/components/country-additional-information.tsx
@@ -9,7 +9,7 @@ import { Card } from 'primereact/card';
 const CountryAdditionalInfo: React.FC = () => {
   const { countryFrom } = useContext(ToConvertContext); // Context for sharing grade and country globally
   return (
-    <div className="text-center">
+    <div className="text-center p-6">
       {countryFrom &&
         (countryFrom.aditionalInfo ||
           countryFrom.url ||
@@ -17,24 +17,26 @@ const CountryAdditionalInfo: React.FC = () => {
           <Card className="text-color-primary text-xl">
             {/* Conditionally render additional information if available */}
             {countryFrom && countryFrom.aditionalInfo && (
-              <p>{countryFrom.aditionalInfo}</p>
+              <p className="px-1">{countryFrom.aditionalInfo}.</p>
             )}
             {countryFrom && countryFrom.url && (
-              <p>
+              <p className="px-1">
                 You can find more information about the grading system in{" "}
                 <a href={countryFrom.url} target="_blank">
                   {countryFrom.label}
                 </a>
+                .
                 <br />
               </p>
             )}
             {countryFrom && countryFrom.url && (
-              <p>
+              <p className="px-1">
                 If you want to see the specific document of conversions you can
                 download it in{" "}
                 <a href={countryFrom.document_url} target="_blank">
                   {countryFrom.label} document
                 </a>
+                .
                 <br />
               </p>
             )}
