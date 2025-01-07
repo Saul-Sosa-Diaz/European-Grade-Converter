6f7cd68692a17af2496c6934c2a5764d91d60c8e
Saul Sosa <saulsosa@acidtango.com>
2025-01-07 11:44:17 +0000

    feat: update useGetCountries hook to return additional query state properties

--- src/hooks/useGetCountries.ts
+++ src/hooks/useGetCountries.ts
@@ -2,11 +2,11 @@ import { useApi } from '@/context/ApiContext'
 import { useQuery } from '@tanstack/react-query'
 
 export const useGetCountries = () => {
-  const {Countries} = useApi()
-  const { data, isLoading } = useQuery({
+  const { Countries } = useApi()
+  const { data, ...rest} = useQuery({
     queryKey: ['countries'],
     queryFn: async () => await Countries.getCountries(),
   })
 
-  return { countries: data, isLoading }
+  return { countries: data, ...rest }
 }
