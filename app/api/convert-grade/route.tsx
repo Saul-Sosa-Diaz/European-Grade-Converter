4c18b3c68a0d4bcf1cbdec81fe134b64873d2b4d
Saul Sosa <saulsosa@acidtango.com>
2025-01-13 13:22:20 +0000

    feat: update convert-grade API to include fixed property and use ConverterDirection enum for grade conversion

--- app/api/convert-grade/route.tsx
+++ app/api/convert-grade/route.tsx
@@ -1,22 +1,18 @@
-import { createDatabaseAdapter } from '@/infrastructure/config/databaseConfig';
+import { ConverterDirection, createDatabaseAdapter } from '@/infrastructure/config/databaseConfig';
 
 export async function POST(request: Request) {
   try {
-    console.log('post /api/convert-grade');
     const body = await request.json();
-    const { grade, fromEvaluationSystemID, toEvaluationSystemID } = body;
+    const { grade, fromEvaluationSystemID, toEvaluationSystemID, fixed } = body;
     if (!grade || !fromEvaluationSystemID || !toEvaluationSystemID) {
       return new Response(JSON.stringify({ message: 'Missing required fields' }), { status: 400 });
     }
-    
+
     const databaseAdapter = createDatabaseAdapter();
-    console.log('grade', grade);
-    console.log('fromEvaluationSystemID', fromEvaluationSystemID);
-    const spainGrade = await databaseAdapter.convertGrade({ evaluationSystemID: fromEvaluationSystemID, grade: parseFloat(grade), direction: 'toSpanish' });
-    console.log('grade', spainGrade);
-    console.log('toEvaluationSystemID', toEvaluationSystemID);
-    const result = await databaseAdapter.convertGrade({ evaluationSystemID: toEvaluationSystemID, grade: parseFloat(spainGrade) });
-    return Response.json({ convertedGrade: result });
+    const spainGrade = await databaseAdapter.convertGrade({ evaluationSystemID: fromEvaluationSystemID, grade: parseFloat(grade), direction: ConverterDirection.toSpain });
+    const result = await databaseAdapter.convertGrade({ evaluationSystemID: toEvaluationSystemID, grade: spainGrade });
+
+    return Response.json({ convertedGrade: result.toFixed(fixed) });
   } catch (error) {
     console.error(error);
     return Response.json({ error });
