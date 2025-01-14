9127654a47d4707c844bae97af189a2581ec4d84
Saul Sosa <saulsosa@acidtango.com>
2025-01-14 13:41:44 +0000

    feat: add evaluation type parameters to grade conversion API and adjust response formatting

--- app/api/convert-grade/route.tsx
+++ app/api/convert-grade/route.tsx
@@ -1,18 +1,23 @@
+import { EvaluationType } from '@/domain/country/country';
 import { ConverterDirection, createDatabaseAdapter } from '@/infrastructure/config/databaseConfig';
 
 export async function POST(request: Request) {
   try {
     const body = await request.json();
-    const { grade, fromEvaluationSystemID, toEvaluationSystemID, fixed } = body;
+    const { grade, fromEvaluationSystemID, toEvaluationSystemID, fixed, fromEvaluationType, toEvaluationType } = body;
     if (!grade || !fromEvaluationSystemID || !toEvaluationSystemID) {
       return new Response(JSON.stringify({ message: 'Missing required fields' }), { status: 400 });
     }
 
     const databaseAdapter = createDatabaseAdapter();
-    const spainGrade = await databaseAdapter.convertGrade({ evaluationSystemID: fromEvaluationSystemID, grade: parseFloat(grade), direction: ConverterDirection.toSpain });
-    const result = await databaseAdapter.convertGrade({ evaluationSystemID: toEvaluationSystemID, grade: spainGrade });
+    const spainGrade = await databaseAdapter.convertGrade({ evaluationSystemID: fromEvaluationSystemID, grade: grade, direction: ConverterDirection.toSpain, evaluationType: fromEvaluationType });
+    const result = await databaseAdapter.convertGrade({ evaluationSystemID: toEvaluationSystemID, grade: spainGrade, evaluationType: toEvaluationType });
 
-    return Response.json({ convertedGrade: result.toFixed(fixed) });
+    if (toEvaluationType === EvaluationType.CONTINUOUS) {
+      return Response.json({ convertedGrade: result.toFixed(fixed) });
+    } else {
+      return Response.json({ convertedGrade: result });
+    }
   } catch (error) {
     console.error(error);
     return Response.json({ error });
