8e991ad4ad25c9f8ad5fcbe746a4d7ce82b740bd
Saul Sosa <saulsosa@acidtango.com>
2025-01-14 13:41:25 +0000

    feat: update grade conversion queries to include discrete grade conversion and fix naming for continuous queries

--- src/infrastructure/database/queries.ts
+++ src/infrastructure/database/queries.ts
@@ -15,20 +15,33 @@ export const QUERIES = {
     JOIN COUNTRY ON UNIVERSITY.countryID = COUNTRY.countryID 
     JOIN EVALUATION_SYSTEM ON UNIVERSITY.universityID = EVALUATION_SYSTEM.universityID
   `,
-  CALCULATE_GRADE_TO_SPAIN: `
+  COUNTINUOUS_TO_SPAIN: `
      SELECT gc.MinIntervalGrade, gc.MaxIntervalGrade,
             gc.baseEquivalentSpanishGrade, gc.topEquivalentSpanishGrade
-            FROM GRADE_CONVERSION gc
+        FROM CONTINUOUS_GRADE_CONVERSION gc
             WHERE gc.evaluationSystemID = $1
-			AND $2 BETWEEN gc.MinIntervalGrade AND gc.MaxIntervalGrade
+			      AND $2 BETWEEN gc.MinIntervalGrade AND gc.MaxIntervalGrade
       ORDER BY gc.MinIntervalGrade DESC
         `,
-  CALCULATE_GRADE_FROM_SPAIN: `
+  COUNTINUOUS_FROM_SPAIN: `
      SELECT gc.MinIntervalGrade, gc.MaxIntervalGrade,
             gc.baseEquivalentSpanishGrade, gc.topEquivalentSpanishGrade
-            FROM GRADE_CONVERSION gc
+        FROM CONTINUOUS_GRADE_CONVERSION gc
             WHERE gc.evaluationSystemID = $1
-			AND $2 BETWEEN gc.baseEquivalentSpanishGrade AND gc.topEquivalentSpanishGrade
+			      AND $2 BETWEEN gc.baseEquivalentSpanishGrade AND gc.topEquivalentSpanishGrade
       ORDER BY gc.baseEquivalentSpanishGrade DESC
         `,
+  DISCRETE_TO_SPAIN: `
+    SELECT dc.gradeValue, dc.baseEquivalentSpanishGrade, dc.topEquivalentSpanishGrade
+            FROM DISCRETE_GRADE_CONVERSION dc
+            WHERE dc.evaluationSystemID = $1
+			      AND dc.gradeValue = $2
+     		ORDER BY dc.baseEquivalentSpanishGrade DESC`,
+  DISCRETE_FROM_SPAIN: `
+    SELECT dc.gradeValue, dc.baseEquivalentSpanishGrade, dc.topEquivalentSpanishGrade
+            FROM DISCRETE_GRADE_CONVERSION dc
+            WHERE dc.evaluationSystemID = $1
+			      AND $2 BETWEEN dc.baseEquivalentSpanishGrade 
+            AND dc.topEquivalentSpanishGrade
+      ORDER BY dc.baseEquivalentSpanishGrade DESC`,
 }
