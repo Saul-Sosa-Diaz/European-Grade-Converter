76965213aefa3bda2396c7c7e1e1aff429220ec3
Saul Sosa <alu0101404141@ull.edu.es>
2025-01-24 12:39:22 +0000

    feat: implement admin layout with sidebar navigation and dynamic tab content

--- src/infrastructure/screens/admin/admin.tsx
+++ src/infrastructure/screens/admin/admin.tsx
@@ -5,10 +5,20 @@
 import { useGetCountryList } from "@/hooks/useGetCountryList";
 import { CountryList } from "./components/CountryList/CountryList";
 import { ProgressSpinner } from "primereact/progressspinner";
+import { HeaderSideBar, MainContainer, MainContent, SideBar } from "./admin.styles";
+import { useState } from "react";
 // TODO: UNCOMMENT THE FOLLOWING LINES
+
+enum AdminTabsNames {
+  COUNTRIES = 'Countries',
+  UNIVERSITIES = 'Universities',
+  EVALUATION_SYSTEM = 'Evaluation Systems'
+}
+
 export const Admin = () => {
   // const { data: session, status } = useSession();
   // const [loading, setLoading] = useState(true);
+  const [activeTab, setActiveTab] = useState(AdminTabsNames.COUNTRIES);
 
   // useEffect(() => {
   //   if (status === "loading") {
@@ -25,8 +35,7 @@ export const Admin = () => {
   //   redirect('/403')
   // }
 
-  const {countryList, isLoading} = useGetCountryList(); 
-  console.log('countryList', countryList);
+  const { countryList, isLoading } = useGetCountryList();
   // return (
   //   <>
   //     {session && (
@@ -42,8 +51,22 @@ export const Admin = () => {
   }
   return (
     <>
-      <h1>Admin Page</h1>
-      <CountryList countryList={countryList} />
+      <MainContainer>
+        <SideBar>
+          <HeaderSideBar><h1>Admin Page</h1 ></HeaderSideBar>
+          <ul>
+            <li onClick={() => { setActiveTab(AdminTabsNames.COUNTRIES) }}>{AdminTabsNames.COUNTRIES}</li>
+            <li onClick={() => { setActiveTab(AdminTabsNames.UNIVERSITIES) }}>{AdminTabsNames.UNIVERSITIES}</li>
+            <li onClick={() => { setActiveTab(AdminTabsNames.EVALUATION_SYSTEM) }}>{AdminTabsNames.EVALUATION_SYSTEM}</li>
+          </ul>
+        </SideBar>
+        <MainContent>
+          {activeTab === AdminTabsNames.COUNTRIES && <CountryList countryList={countryList} />}
+          {activeTab === AdminTabsNames.UNIVERSITIES && <h1>Universities</h1>}
+          {activeTab === AdminTabsNames.EVALUATION_SYSTEM && <h1>Evaluation System</h1>}
+        </MainContent>
+
+      </MainContainer>
     </>
   );
 };
