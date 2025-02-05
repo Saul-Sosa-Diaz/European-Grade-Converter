f327f622800baa1cb5598821cc436e22c33e6e58
Saul Sosa <alu0101404141@ull.edu.es>
2025-02-05 19:35:43 +0000

    feat: remove error boundary and custom 500 error page from main app component

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,10 @@
 "use client";
-import { Custom500 } from "@/infrastructure/screens/500/custom500";
 import { Home as HomeScreen } from "@/infrastructure/screens/home";
-import { ErrorBoundary } from "react-error-boundary";
 
 
 export default function App() {
 
   return (
-    <ErrorBoundary FallbackComponent={Custom500}>
       <HomeScreen />
-    </ErrorBoundary>
   );
 }
