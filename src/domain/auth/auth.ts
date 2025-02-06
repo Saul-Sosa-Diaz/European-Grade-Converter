555a019d24b6a23f060837de12adb72856c73dcc
Saul Sosa <alu0101404141@ull.edu.es>
2025-02-06 17:19:36 +0000

    feat: extend Credentials type with apiKey and roleName, add ResponseCredentials enum

--- src/domain/auth/auth.ts
+++ src/domain/auth/auth.ts
@@ -1,6 +1,12 @@
 export type Credentials = {
   username: string
   password: string
+  apiKey: string
+  roleName: string
+}
+
+export enum ResponseCredentials {
+  INVALID_CREDENTIALS = 'Invalid credentials',
 }
 
 export type User = {
