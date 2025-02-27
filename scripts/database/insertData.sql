d8f56faf0c4dc6fa8c09b399a900971e12aa5c95
Saul Sosa <alu0101404141@ull.edu.es>
2025-02-27 19:00:12 +0000

    Feat: Add Ireland evaluation system and continuous grade conversion entries to the database

--- scripts/database/insertData.sql
+++ scripts/database/insertData.sql
@@ -1044,3 +1044,89 @@ VALUES (
    5,
    'F'           
 );
+
+-- Ireland
+INSERT INTO EVALUATION_SYSTEM (
+   universityID, evaluationSystemName, evaluationType, validGrades, fixed, evaluationsysteminfo, urlToEvidence
+)
+VALUES (
+  5,
+  'Ireland',
+  'continuous',
+  '{"0","1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19","20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31", "32", "33", "34", "35", "36", "37", "38", "39", "40", "41", "42", "43", "44", "45", "46", "47", "48", "49", "50", "51", "52", "53", "54", "55", "56", "57", "58", "59", "60", "61", "62", "63", "64", "65", "66", "67", "68", "69", "70", "71", "72", "73", "74", "75", "76", "77", "78", "79", "80", "81", "82", "83", "84", "85", "86", "87", "88", "89", "90", "91", "92", "93", "94", "95", "96", "97", "98", "99", "100"}',
+   0,
+  'Insert a value between 0 and 100',
+  'https://www.irelandassignmenthelp.com/blogs/grading-system-ireland/'
+);
+INSERT INTO CONTINUOUS_GRADE_CONVERSION (
+   evaluationSystemID, MinIntervalGrade, MaxIntervalGrade, baseEquivalentSpanishGrade, topEquivalentSpanishGrade, gradeName, europeanEquivalence
+)
+VALUES (
+  14,               
+  0,               
+  40,               
+  0,                               
+  5,            
+  'Fail',
+  'F'
+);
+INSERT INTO CONTINUOUS_GRADE_CONVERSION (
+   evaluationSystemID, MinIntervalGrade, MaxIntervalGrade, baseEquivalentSpanishGrade, topEquivalentSpanishGrade, gradeName, europeanEquivalence
+)
+VALUES (
+14, 
+  40,               
+  45,               
+  5.0,                               
+  6,            
+  'Sufficent',
+   'E'
+);
+INSERT INTO CONTINUOUS_GRADE_CONVERSION (
+   evaluationSystemID, MinIntervalGrade, MaxIntervalGrade, baseEquivalentSpanishGrade, topEquivalentSpanishGrade, gradeName, europeanEquivalence
+)
+VALUES (
+14, 
+  45,               
+  50,               
+  6.0,                             
+  7,            
+  'Satisfactory',
+   'D'
+);
+INSERT INTO CONTINUOUS_GRADE_CONVERSION (
+   evaluationSystemID, MinIntervalGrade, MaxIntervalGrade, baseEquivalentSpanishGrade, topEquivalentSpanishGrade, gradeName, europeanEquivalence
+)
+VALUES (
+14, 
+  50,               
+  60,               
+  7.0,                               
+  8,            
+  'Good',
+   'C'
+);
+INSERT INTO CONTINUOUS_GRADE_CONVERSION (
+   evaluationSystemID, MinIntervalGrade, MaxIntervalGrade, baseEquivalentSpanishGrade, topEquivalentSpanishGrade, gradeName, europeanEquivalence
+)
+VALUES (
+14, 
+  60,               
+  70,               
+  8.0,                               
+  9,            
+  'Very Good',
+   'B'
+);
+INSERT INTO CONTINUOUS_GRADE_CONVERSION (
+   evaluationSystemID, MinIntervalGrade, MaxIntervalGrade, baseEquivalentSpanishGrade, topEquivalentSpanishGrade, gradeName, europeanEquivalence
+)
+VALUES (
+  14, 
+  70,               
+  100,               
+  9,                               
+  10,            
+  'Excellent',
+   'A'
+);
