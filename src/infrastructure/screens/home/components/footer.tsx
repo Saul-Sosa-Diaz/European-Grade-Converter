edbff1bd84e057731e0c0edc0cdd89acbade49ed
Saul Sosa <alu0101404141@ull.edu.es>
2025-03-24 13:53:37 +0000

    Feat: Update footer contact information to reflect new supervisor email

--- src/infrastructure/screens/home/components/footer.tsx
+++ src/infrastructure/screens/home/components/footer.tsx
@@ -14,19 +14,11 @@ export const Footer = () => {
   return (
     <footer className="flex block justify-content-center w-screen sticky bottom-0 color-primary">
       <p className="w-11 text-white text-center line-height-3">
-        If you find an error or have a suggestion, please inform
-        <strong>
-          {" "}
-          Saúl Sosa Díaz (
-          <a href="mailto:alu0101404141@ull.edu.es">alu0101404141@ull.edu.es</a>
-          )
-        </strong>
-        . This project is being supervised by{" "}
+        If you find an error or have a suggestion, please contact with {" "}
         <strong>
           Prof. Juan José Salazar González (
           <a href="mailto:jjsalaza@ull.edu.es">jjsalaza@ull.edu.es</a>)
-        </strong>{" "}
-        at Universidad de La Laguna.
+        </strong>
       </p>
     </footer>
   );
